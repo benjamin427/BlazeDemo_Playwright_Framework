8e0b686bf3cb19b01f714d6ee33f836ef7401a6a
benjamin427 <shadowboxer79@hotmail.com>
2026-01-06 22:53:04 -0500

    Included more negative testing scenarios

--- test/negative_login.spec.js
+++ test/negative_login.spec.js
@@ -1,6 +1,11 @@
 const {expect, test} = require('@playwright/test')
 const {blazedemo_login} = require('../pageObjects/pageobject_login')
 
+
+test.beforeEach(async({page}) => {
+    const login = new blazedemo_login(page)
+    await login.visitWebsite()
+})
 test("Should return an error prompt message on the empty email text field after clicking the button ", async({page}) => {
     //Environment variables from the .env.login file
     const title = process.env.LOGIN_TITLE
@@ -9,11 +14,10 @@ test("Should return an error prompt message on the empty email text field after 
 
     //Accessing the page object to use the targeted elements for executiong the test
     const login = new blazedemo_login(page)
-    await login.visitWebsite()
     await expect(page).toHaveURL(endpoint_url)
     await expect(page).toHaveTitle(title)
     await login.negative_test_email_address_login(password)
-    await login.close()
+
 })
 test("Should return an error prompt message on the empty password text field after clicking the button", async({page}) => {
     //Environment variables from the .env.login file
@@ -23,9 +27,21 @@ test("Should return an error prompt message on the empty password text field aft
 
     //Accessing the page object to use the targeted elements for executiong the test
     const login = new blazedemo_login(page)
-    await login.visitWebsite()
     await expect(page).toHaveURL(endpoint_url)
     await expect(page).toHaveTitle(title)
     await login.negative_test_password(emailAddress)
+
+})
+test("Should return an error prompt messsage indictaing the user to fill out the login form", async({page}) => {
+    const login = new blazedemo_login(page)
+    const title = process.env.LOGIN_TITLE
+    const endpoint_url = process.env.LOGIN_ENDPOINT
+    await expect(page).toHaveURL(endpoint_url)
+    await expect(page).toHaveTitle(title)
+    await login.negative_test_login()
+})
+test.afterEach(async({page}) => {
+    const login = new blazedemo_login(page)
     await login.close()
 })
+
