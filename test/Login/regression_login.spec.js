bdd34dfc9cd1a049ea9711bf713fd67fb5aa576f
benjamin427 <shadowboxer79@hotmail.com>
2026-01-08 10:26:42 -0500

    Created updates for this test suite

---
+++ test/Login/regression_login.spec.js
@@ -0,0 +1,20 @@
+const {expect, test} = require('@playwright/test')
+const {blazedemo_login} = require('./pageObject/pageobject_login')
+
+
+test("Regression test of the login form", async ({page}) => {
+  // Environment variables from the .env.login file
+  const email_address = process.env.LOGIN_EMAIL
+  const password = process.env.LOGIN_PASSWORD
+  const title = process.env.LOGIN_TITLE
+  const endpoint_url = process.env.LOGIN_ENDPOINT
+
+  //Accessing the page object to use the targeted elements for executiong the test
+  const login = new blazedemo_login(page)
+  await login.visitWebsite()
+  await expect(page).toHaveURL(endpoint_url)
+  await expect(page).toHaveTitle(title)
+  await login.positive_test_login(email_address, password)
+  
+})
+
