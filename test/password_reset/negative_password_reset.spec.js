0d1f3bc7a2fba2224468338313ba34e58f878936
benjamin427 <shadowboxer79@hotmail.com>
2026-01-08 11:49:53 -0500

    Created updates for this test suite

---
+++ test/password_reset/negative_password_reset.spec.js
@@ -0,0 +1,17 @@
+const {expect, test} = require('@playwright/test')
+const {blazedemo_passwordreset} = require('./page_object/pageobject_passwordreset')
+
+test("Should return an error prompt message of the empty text field after clicking the button", async({page}) => {
+    //Environment variables from the .env.passwordReset file
+    const email_address = process.env.PASSWORD_RESET_EMAIL
+    const title = process.env.PASSWORD_RESET_TITLE
+    const endpoint_url = process.env.PASSWORD_RESET_ENDPOINT
+
+    //Accessing the page object to use the targeted elements for executiong the test
+    const passwordReset = new blazedemo_passwordreset(page)
+    await passwordReset.vistiWebsite()
+    await expect(page).toHaveTitle(title)
+    await expect(page).toHaveURL(endpoint_url)
+    await passwordReset.negative_test_reset_Password()
+    await passwordReset.close()
+})
---
+++ test/password_reset/page_object/pageobject_passwordreset.js
@@ -0,0 +1,22 @@
+// Page object for the user to reset password
+exports.blazedemo_passwordreset = class BlazeDemo_Password_Reset{
+    constructor(page){
+        this.page = page
+        this.emailAddress = page.getByRole("textbox", {name: "E-Mail Address"})
+        this.submitButton = page.getByRole("button", {name: "Send Password Reset Link"})
+    }
+    async vistiWebsite(){
+        await this.page.goto("https://blazedemo.com/password/reset")
+    }
+    async positive_test_reset_Password(email_address){
+        await this.emailAddress.fill(email_address)
+        await this.submitButton.click()
+    }
+    async negative_test_reset_Password(){
+        await this.submitButton.click()
+    }
+    async close(){
+        await this.page.close()
+    }
+}
+
